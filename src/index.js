d45d56cbd3f27ac5361f345c8460e55c8cead688
Oleg Bely <welcome.bely@gmail.com>
2018-05-06 19:57:14 +0300

    set up environment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Component() {
+    return (
+        <div>I'm component</div>
+    )
+}
+
+ReactDOM.render(<Component />, document.getElementById('root'));
+
